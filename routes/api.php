14773d4f8464c7e46337d6bbf0076cb6ef83c97c
Juan Camilo Valencia Mosquera <jcvm2001valencia@gmail.com>
2025-05-26 22:34:51 -0500

    cambio 2

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
 
 // === INCLUSIÓN DEL CONTROLADOR ===
-require_once __DIR__ . '/./controllers/productocontroller.php';
+require_once __DIR__ . '../controllers/productocontroller.php';
 $controller = new ProductoController();
 
 // === ENTRADA JSON (para POST, PUT, DELETE) ===
